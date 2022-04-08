50de8d3968aafde1c9d77aac7649c8522cd41220
blind-contours <david_mccoy@berkeley.edu>
2022-04-08 11:30:11 -0700

    all slurm scripts

---
+++ slurm/sbatch_day100_deaths.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=covid_deaths_day100_modeling
+#
+# Partition:
+#SBATCH --partition=savio2
+#
+
+#SBATCH --qos=biostat_savio2_normal
+#SBATCH --account=co_biostat
+
+# Number of nodes for use case:
+#SBATCH --nodes=1
+#
+# Wall clock limit:
+#SBATCH --time 4:00:00
+#
+## Command(s) to run (example):
+module load r/4.0.3
+OMP_NUM_THREADS=1
+### for foreach+doSNOW ###
+cd /global/scratch/users/david_mccoy/COVIDxrisk/COVIDxrisk
+ 
+R CMD BATCH --no-save R/03_fit_day100_deaths_model.R logs/deaths_day100_modeling.Rout
